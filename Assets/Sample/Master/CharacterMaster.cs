ac8dde034e958d6af8b9803ec72378be8ec9ecb0
ducchy <nozu_soichiro@grenge.co.jp>
2023-03-05 13:38:39 +0900

    sample追加

---
+++ Assets/Sample/Master/CharacterMaster.cs
@@ -0,0 +1,10 @@
+using SpreadSheetMaster;
+
+namespace sample
+{
+	public partial class CharacterMaster : ImportableSpreadSheetMasterBase<CharacterMasterData>
+	{
+		public override string spreadSheetId => "1JVNBk5FbMS75I-l9ua1e2lbZRry4Q7kvrf7Kfu267bk";
+		public override string sheetName => "character";
+	}
+}
---
+++ Assets/Sample/Sample.cs
@@ -0,0 +1,46 @@
+using System.Text;
+using System.Threading;
+using SpreadSheetMaster;
+using UnityEngine;
+
+namespace sample
+{
+	public class Sample : MonoBehaviour
+	{
+		private readonly CharacterMaster _characterMaster = new CharacterMaster();
+		private readonly SpreadSheetMasterImporter _importer = new SpreadSheetMasterImporter();
+
+		private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+		private CancellationToken ct => _cts.Token;
+
+
+		private void Start()
+		{
+			ImportMasterAllAsync();
+		}
+
+		private async void ImportMasterAllAsync()
+		{
+			await _importer.ImportFromSpreadSheetAsync(_characterMaster, OnError, ct);
+			OnCompletedImport();
+		}
+
+		private void OnError(string error)
+		{
+			Debug.LogError(error);
+		}
+
+		private void OnCompletedImport()
+		{
+			var sb = new StringBuilder();
+			sb.Append("[インポート完了]").AppendLine();
+
+			var characters = _characterMaster.datas;
+			sb.AppendFormat("CharacterMaster: count={0}", characters.Count).AppendLine();
+			foreach (var character in characters)
+				sb.AppendFormat("- {0}: {1}", character.id, character.name).AppendLine();
+
+			Debug.Log(sb.ToString());
+		}
+	}
+}
