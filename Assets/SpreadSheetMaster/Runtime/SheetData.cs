dedebd59fc19f0d48825b291e34f92195fee4c38
ducchy <nozu_soichiro@grenge.co.jp>
2023-03-14 19:18:52 +0900

    スプシ設定ファイルに対応

---
+++ Assets/SpreadSheetMaster/Runtime/SheetData.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SpreadSheetMaster
+{
+    [Serializable]
+    public class SheetData
+    {
+        public string name;
+        public string id;
+        public string masterName;
+    }
+}
