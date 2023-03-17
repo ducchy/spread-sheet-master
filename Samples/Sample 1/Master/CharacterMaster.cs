90629ac5b942d794b8c219c7a2616725413dee63
ducchy <nozu_soichiro@grenge.co.jp>
2023-03-17 15:55:40 +0900

    v1.3.8

--- Samples/Sample 1/Master/CharacterMaster.cs
+++ Samples/Sample 1/Master/CharacterMaster.cs
@@ -5,7 +5,7 @@ namespace SpreadSheetMaster.Samples
 	public partial class CharacterMaster : ImportableSpreadSheetMasterBase<CharacterMasterData>
 	{
 		protected override string defaultSpreadSheetId => "1qUJ3j7djNezcfvet3p7WcrJ2NS0cQlthybEIjGjK6As";
-		public override string sheetId => "1353480984";
+		public override string sheetId => "";
 		public override string sheetName => "キャラクター";
 	}
 }
