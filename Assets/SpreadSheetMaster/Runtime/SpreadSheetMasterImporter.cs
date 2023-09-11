5fc585618b03b3e3403130f17943d6750b4282c2
ducchy <dycchy.gamedev@gmail.com>
2023-09-11 09:29:32 +0900

    refactor: CsvLoadロジックのInterface化

--- Assets/SpreadSheetMaster/Runtime/SpreadSheetMasterImporter.cs
+++ Assets/SpreadSheetMaster/Runtime/SpreadSheetMasterImporter.cs
@@ -1,91 +1,41 @@
 using System;
-using System.Threading.Tasks;
 using System.Threading;
-using UnityEngine;
 
 namespace SpreadSheetMaster
 {
     /// <summary> スプレッドシートインポート </summary>
     public class SpreadSheetMasterImporter
     {
-        /// <summary> パーサー </summary>
+        /// <summary> csvパーサー </summary>
         private readonly ICsvParser _parser;
 
         /// <summary> ログレベル </summary>
         private readonly LogLevel _logLevel;
 
-        /// <summary> シートダウンローダー </summary>
-        private readonly SheetDownloader _sheetDownloader = new();
-
         /// <summary> コンストラクタ </summary>
-        public SpreadSheetMasterImporter(ICsvParser parser, LogLevel logLevel)
+        public SpreadSheetMasterImporter(
+            ICsvParser parser,
+            LogLevel logLevel)
         {
             _parser = parser;
             _logLevel = logLevel;
         }
 
-        /// <summary> スプレッドシートからインポート </summary>
-        public AsyncOperationHandle<ImportMasterLogBuilder> ImportFromSpreadSheetAsync(
+        /// <summary> インポート </summary>
+        public AsyncOperationHandle<ImportMasterLogBuilder> ImportAsync(
             IImportableSpreadSheetMaster master,
-            string spreadSheetId,
-            string sheetId,
+            string csv,
             CancellationToken token)
         {
             var op = new AsyncOperator<ImportMasterLogBuilder>();
 
-            var sheetDownloadHandle = _sheetDownloader.DownloadSheetAsync(spreadSheetId, sheetId, token);
-            ImportFromSpreadSheetInternalAsync(op, sheetDownloadHandle, master, token)
-                .ContinueWith(_ => { }, token);
+            ImportFromCsv(op, master, csv);
+
             return op;
         }
 
-        /// <summary> リソースからインポート </summary>
-        public AsyncOperationHandle<ImportMasterLogBuilder> ImportFromResource(
-            IImportableSpreadSheetMaster master,
-            string resourcePath)
-        {
-            var op = new AsyncOperator<ImportMasterLogBuilder>();
-
-            var csvFile = Resources.Load<TextAsset>(resourcePath);
-            if (csvFile == null)
-            {
-                op.Canceled(new InvalidOperationException($"Failed to load resource: path={resourcePath}"));
-                return op;
-            }
-
-            return ImportFromCsv(op, master, csvFile.text);
-        }
-
-        /// <summary> スプレッドシートからインポート </summary>
-        private async Task ImportFromSpreadSheetInternalAsync(AsyncOperator<ImportMasterLogBuilder> op,
-            AsyncOperationHandle<string> sheetDownloadHandle,
-            IImportableSpreadSheetMaster master,
-            CancellationToken token)
-        {
-            while (!sheetDownloadHandle.IsDone)
-            {
-                if (token.IsCancellationRequested)
-                {
-                    token.ThrowIfCancellationRequested();
-                    op.Canceled();
-                    return;
-                }
-
-                await Task.Yield();
-            }
-
-            if (sheetDownloadHandle.Exception != null)
-            {
-                op.Canceled(sheetDownloadHandle.Exception);
-                return;
-            }
-
-            var csv = sheetDownloadHandle.Result;
-            ImportFromCsv(op, master, csv);
-        }
-
         /// <summary> CSVからインポート </summary>
-        private AsyncOperationHandle<ImportMasterLogBuilder> ImportFromCsv(
+        private void ImportFromCsv(
             AsyncOperator<ImportMasterLogBuilder> op,
             IImportableSpreadSheetMaster master,
             string csv)
@@ -93,10 +43,13 @@ namespace SpreadSheetMaster
             if (string.IsNullOrEmpty(csv))
             {
                 op.Canceled(new InvalidOperationException("csv is empty"));
-                return op;
+                return;
             }
 
+            // パース
             var records = _parser.Parse(csv, excludeHeader: true);
+
+            // インポート
             var importInfo = new ImportMasterLogBuilder();
             importInfo.Initialize(master.className, records.Count, _logLevel);
             try
@@ -105,15 +58,15 @@ namespace SpreadSheetMaster
             }
             catch (Exception e)
             {
+                // インポート失敗
                 importInfo.Exception(e);
                 op.Canceled(e);
-                return op;
+                return;
             }
 
+            // インポート成功
             importInfo.ExportLog();
             op.Completed(importInfo);
-
-            return op;
         }
     }
 }
